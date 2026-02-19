33ad86395399bf8498e5f177404e27b809170b8b
panoli1414 <larocachikito15@gmail.com>
2026-02-19 09:14:08 -0600

    Add files via upload

--- LaragonSQL.430.sql
+++ LaragonSQL.430.sql
@@ -3,6 +3,7 @@ CREATE DATABASE Escuela;
 CREATE DATABASE Negocio;
 CREATE DATABASE Jugueteria;
 CREATE DATABASE Tienda;
+CREATE DATABASE Biblioteca;
 
 -- Seleccionamos la base de datos escuela que vamos a utilizar
 alumno
@@ -18,3 +19,21 @@ Direccion VARCHAR(50),
 Telefono VARCHAR(15),
 PRIMARY KEY(NC)
 );
+USE Biblioteca;
+CREATE TABLE libro(
+NL INT,
+Titulo VARCHAR(50),
+edicion VARCHAR(30),
+Fecha VARCHAR(30),
+Semestre INT,
+PRIMARY KEY(NL)
+);
+USE Biblioteca;
+CREATE TABLE Autor(
+NA INT,
+Nombre VARCHAR(50),
+Paisorigen VARCHAR(30),
+genero VARCHAR(30),
+PRIMARY KEY(NA)
+);autor
+
